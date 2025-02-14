ea189cf321e5a187e39020bda49e33170bc37215
CrystalNyein <nyeinchanlay99@gmail.com>
2025-02-14 13:15:27 +0630

    created manifest file

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+  'name':'Custom Product module',
+  'version':'1.0',
+  'author':'Nyein Chan Lay',
+  'depends':['base'],
+  'data':[
+    'security/ir.model.access.csv',
+    'views/custom_product_views.xml',
+  ],
+  'installable':True,
+  'application':True
+}
